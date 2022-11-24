53b4b6ce73f68a0c68a2084bdbda1034b29c4dd3
Gregory Saimoen <greg.eres.07@hotmail.com>
2022-11-24 19:50:39 +1100

    ajout de la suppression

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { RequestService } from './service/request.service';
 import { Component, OnInit } from '@angular/core';
+import { Employees } from './model/employees.model';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +10,45 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'employee_app';
 
-  employee: any;
+  _employesArray: Employees[] = [];
+
+  id: number | undefined;
+
+  employee: Employees | undefined;
+
+  message = '';
 
   constructor(private requestService: RequestService) {}
 
   ngOnInit(): void {
     this.requestService.getAll().subscribe((item: any) => {
       console.log(item);
-      this.employee = item;
+      this._employesArray = item;
     });
+  }
+
+  deleteItem(id: any) {
+    this.requestService.delete(id).subscribe(
+      (data) => {
+        console.log(data);
+        // this will return a new array of employees after removing the employee given by the id
+        this._employesArray = this._employesArray.filter(
+          (employee) => employee.id !== id
+        );
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  updateInfo(id: any): void {
+    this.requestService.update(id, this._employesArray).subscribe(
+      (response) => {
+        console.log(response);
+        this.message = 'The tutorial was updated successfully!';
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
